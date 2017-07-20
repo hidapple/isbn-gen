412184fe6c87a4458110eb4a7b386f65bdf1eecd
hidapple <shotfsh@gmail.com>
2017-07-20 23:59:58 +0900

    Generate rondom isbn

--- cli.go
+++ cli.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"math/rand"
 	"strconv"
+	"strings"
 )
 
 const (
@@ -27,7 +28,7 @@ func (cli *CLI) Run(args []string) int {
 	flags := flag.NewFlagSet(Name, flag.ContinueOnError)
 	flags.SetOutput(cli.errStream)
 
-	flags.StringVar(&pubcode, "-pubcode", "0", "Publisher code of ISBN")
+	flags.StringVar(&pubcode, "pubcode", "0", "Publisher code of ISBN")
 	flags.StringVar(&pubcode, "p", "0", "Publisher code of ISBN (Short)")
 
 	flags.BoolVar(&version, "version", false, "Print version information and quit.")
@@ -44,13 +45,18 @@ func (cli *CLI) Run(args []string) int {
 	}
 
 	// Generate ISBN
-	isbn := generate(pubcode)
-	fmt.Print(isbn)
+	fmt.Println(generate(pubcode))
 
+	// Succeeded
 	return ExitCodeOK
 }
 
 func generate(pubcode string) string {
+	isbn := generate12digit(pubcode)
+	return isbn + strconv.Itoa(calcCheckDigit(isbn))
+}
+
+func generate12digit(pubcode string) string {
 	const JapanIsbnPrefix = "9784"
 	isbn := JapanIsbnPrefix + pubcode
 	length := 8 - len(pubcode)
@@ -58,4 +64,23 @@ func generate(pubcode string) string {
 		isbn += strconv.Itoa(rand.Intn(10))
 	}
 	return isbn
+}
+
+func calcCheckDigit(partOfIsbn string) int {
+	sum := 0
+	for i, v := range strings.Split(partOfIsbn, "") {
+		intV, _ := strconv.Atoi(v)
+		if i%2 == 0 {
+			sum += intV
+		} else if i%2 == 1 {
+			sum += intV * 3
+		}
+	}
+
+	calcResult := 10 - (sum % 10)
+	if calcResult == 10 {
+		return 0
+	} else {
+		return calcResult
+	}
 }
