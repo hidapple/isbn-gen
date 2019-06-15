21763563706219cc6d90e3d4db2d7169397e0b98
hidapple <shotfsh@gmail.com>
2019-06-16 02:45:17 +0900

    Break ISBN struct field into code parts

--- isbn/isbn_test.go
+++ isbn/isbn_test.go
@@ -7,7 +7,7 @@ import (
 func TestNewISBN_Init(t *testing.T) {
 	tests := []struct {
 		groupCode string
-		pubCode   string
+		code      string
 	}{
 		{"Japan", ""},
 		{"jp", ""},
@@ -16,9 +16,9 @@ func TestNewISBN_Init(t *testing.T) {
 	}
 
 	for i, tt := range tests {
-		if _, err := NewISBN(tt.groupCode, tt.pubCode); err != nil {
-			t.Errorf("case %d: ISBN is expected to be able to init with grupCode=%q, pubCode=%q",
-				i, tt.groupCode, tt.pubCode)
+		if _, err := NewISBN(tt.groupCode, tt.code); err != nil {
+			t.Errorf("case %d: ISBN is expected to be able to init with grupCode=%q, code=%q",
+				i, tt.groupCode, tt.code)
 		}
 	}
 }
@@ -26,7 +26,7 @@ func TestNewISBN_Init(t *testing.T) {
 func TestNewISBN_CannotInit(t *testing.T) {
 	tests := []struct {
 		groupCode string
-		pubCode   string
+		code      string
 	}{
 		{"Japan", "000000000"},
 		{"jp", "000000000"},
@@ -34,9 +34,21 @@ func TestNewISBN_CannotInit(t *testing.T) {
 	}
 
 	for i, tt := range tests {
-		if _, err := NewISBN(tt.groupCode, tt.pubCode); err == nil {
-			t.Errorf("case %d: ISBN should not be able to init with groupCode=%q, pubCode=%q",
-				i, tt.groupCode, tt.pubCode)
+		if _, err := NewISBN(tt.groupCode, tt.code); err == nil {
+			t.Errorf("case %d: ISBN should not be able to init with groupCode=%q, code=%q",
+				i, tt.groupCode, tt.code)
 		}
+	}
+}
+
+func TestNumber(t *testing.T) {
+	isbn, err := NewISBN("jp", "")
+	if err != nil {
+		t.Fatalf("failed initializing ISBN")
+	}
+
+	actual := isbn.Number()
+	if len(actual) != 13 {
+		t.Fatalf("expect Number() returns 13 digit numbers but got %q", actual)
 	}
 }
